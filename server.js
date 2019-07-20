77912dcb139d84b630eb459b971fee1faee9b5f2
Jason Peng <jason.j.peng@gamil.com>
2019-07-19 18:07:18 -0700

    /api/friends

--- server.js
+++ server.js
@@ -14,67 +14,53 @@ app.use(express.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
-// var characters = [
-//   {
-//     routeName: "yoda",
-//     name: "Yoda",
-//     role: "Jedi Master",
-//     age: 900,
-//     forcePoints: 2000
-//   },
-//   {
-//     routeName: "darthmaul",
-//     name: "Darth Maul",
-//     role: "Sith Lord",
-//     age: 200,
-//     forcePoints: 1200
-//   },
-//   {
-//     routeName: "obiwankenobi",
-//     name: "Obi Wan Kenobi",
-//     role: "Jedi Master",
-//     age: 55,
-//     forcePoints: 1350
-//   }
-// ];
+var characters = [
+  {
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  },
+  {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  },
+  {
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
+  }
+];
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname , "public/", "home.html"));
-});
-
 app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname , "public/", "survey.html"));
   });
- 
 
 
 app.get("/add", function(req, res) {
   res.sendFile(path.join(__dirname, "add.html"));
 });
 
-// Displays all characters
-app.get("/api/characters", function(req, res) {
+// Displays all possible friends
+app.get("/api/friends", function(req, res) {
   return res.json(characters);
 });
 
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname , "public/", "home.html"));
+  });
 
-  console.log(chosen);
 
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
-});
 
 // Create New Characters - takes in JSON input
 app.post("/api/characters", function(req, res) {
