a8ad175584f3d6b7192ff13cd096e0a4d89c6005
Jason Peng <jason.j.peng@gamil.com>
2019-07-20 22:03:45 -0700

    preview image

--- public/app.js
+++ public/app.js
@@ -15,9 +15,9 @@ strQ = [
 
 $( document ).ready(function() {
     console.log( "ready!" );
-    var userData = {"name": "Jason", "image": "hhhhhhhh",
-        "scores":[1,2,3,4,5,4,3,2,1,4]                               
-    };
+    // var userData = {"name": "Jason", "image": "hhhhhhhh",
+    //     "scores":[1,2,3,4,5,4,3,2,1,4]                               
+    // };
 
 
 var htmlQesAns ="";
@@ -43,11 +43,41 @@ htmlQesAns =
 $("#qTable").append($(htmlQesAns));
 }
 
+$("input[name='image']").change(function(){
+    var imgUrl = $("input[name='image']").val().trim(" ");
+    if (imgUrl ==="") {
+        $("#previewImg img").attr("height", "0")
+    } else {
+       $("#previewImg img").attr("src", imgUrl ).attr('width', "200px").attr('height', "200px");
+    }   
+})
+
 $("#submit").on("click", function(event) {
     event.preventDefault();
+
+    var userData = {
+        name: $("input[name='name']").val(),
+        image: $("input[name='image']").val(),
+        scores: [
+          $("input[name='1']:checked").val(),
+          $("input[name='2']:checked").val(),
+          $("input[name='3']:checked").val(),
+          $("input[name='4']:checked").val(),
+          $("input[name='5']:checked").val(),
+          $("input[name='6']:checked").val(),
+          $("input[name='7']:checked").val(),
+          $("input[name='8']:checked").val(),
+          $("input[name='9']:checked").val(),
+          $("input[name='10']:checked").val()
+        ]
+      };
+
+    
+
     $.post("/api/friends", userData, function(data) {
 
         console.log(data);
+        console.log(userData);
 
         // Grab the result from the AJAX post so that the best match's name and photo are displayed.
         $("#match-name").text(data.name);
